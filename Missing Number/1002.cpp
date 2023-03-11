e11212bb689acadc24862d51e87e5a105fb3c6cf
Jai Singh <jai20070@iiitd.ac.in>
2023-03-11 08:08:06 +0530

    New Changes

--- Missing Number/1002.cpp
+++ Missing Number/1002.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
-#include <vector> 
+#include <vector>
+using namespace std; 
+
+void solve(){
+    int num;
+    cin>>num;
+    vector<int> arr(num,0);
+
+    for(int i=0;i<num-1;i++){
+        int n;
+        cin>>n;
+        arr[n-1]=1;
+    }
+
+    
+}
 
 int main(){
-    
+    solve();
     return 0;
 }
